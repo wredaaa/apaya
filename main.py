9d63a205f66586c853d0ac2d0e3bd95bf1e4f4c7
Wreda Prasetiyo W <wredaaa@gmail.com>
2022-01-22 13:57:08 +0700

    add page login

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
 @app.route("/home")
 def home():
   return render_template('home.html')
@@ -12,4 +16,4 @@ def about():
   return render_template('profile.html')
 
 if __name__ == '__main__':
-  app.run(debug=True) 
+  app.run(debug=True, port=5001) 
