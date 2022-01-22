ea31ad257e568d15fa4d9f490dc1e7e0dccd4bd5
Wreda Prasetiyo W <wredaaa@gmail.com>
2022-01-22 14:07:45 +0700

    add page login #2

--- main.py
+++ main.py
@@ -15,5 +15,9 @@ def home():
 def about():
   return render_template('profile.html')
 
+@app.route("/project_detail")
+def detail():
+  return render_template('detail_project.html')
+
 if __name__ == '__main__':
   app.run(debug=True, port=5001) 
